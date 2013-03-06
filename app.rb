0bc51f3d3175c7fe4da7f79718b93f5491c40160
Tanner Mares <tannermares@gmail.com>
2013-03-06 01:02:05 -0800

    uninitialized constant

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'mechanize'
 
 get '/' do
-	agent = WWW::Mechanize.new
+	agent = Mechanize.new
   	agent.get('http://stackoverflow.com/questions/tagged/ruby')
   	@questions = agent.page.search('.question-hyperlink')
   	erb :index
