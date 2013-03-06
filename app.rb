58c223985ca07d64d25fd33a9dbea96879145085
Tanner Mares <tannermares@gmail.com>
2013-03-06 13:46:12 -0800

    added git questions and some css

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'mechanize'
+require 'bootstrap'
 
 get '/' do
 	agent = Mechanize.new
   	agent.get('http://stackoverflow.com/questions/tagged/ruby')
+  	@rubyQuestions = agent.page.search('.question-hyperlink')
+  	agent.get('http://stackoverflow.com/questions/tagged/git')
   	@questions = agent.page.search('.question-hyperlink')
   	erb :index
 end
